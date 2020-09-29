e17ed3665b19b65effb169f0ac74f532d3d57d90
Matthew Titmus <matthew.titmus@gmail.com>
2020-09-29 13:04:15 -0400

    WIP

--- sentinel_fmt.sh
+++ sentinel_fmt.sh
@@ -1,30 +1,7 @@
 #!/usr/bin/env bash
-set -eux
 
-# for file_with_path in "$@"; do
-#   file_with_path="${file_with_path// /__REPLACED__SPACE__}"
-
-#   paths[index]=$(dirname "$file_with_path")
-
-#   let "index+=1"
-# done
-
-# for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
-#   path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
-
-#   pushd "$path_uniq" > /dev/null
-#   sentinel fmt *.sentinel
-#   popd > /dev/null
-# done
-
-# # sentinel.tfvars are excluded by `sentinel fmt`
-# for tfvars_file in "${tfvars_files[@]}"; do
-#   tfvars_file="${tfvars_file//__REPLACED__SPACE__/ }"
-
-#   echo $tfvars_file"
-#   sentinel fmt "$tfvars_file"
-# done
+set -e
 
 for f in "$@"; do
-    sentinel fmt "$f" 2>&1
+    sentinel fmt "$f"
 done
